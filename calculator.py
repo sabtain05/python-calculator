326b150da909bbafe5e5861c024c251372145251
Sabtain Ali <sabtainalipk144@gmail.com>
2026-08-25 16:31:36 +0500

    Implement multiply and division functions in calculator

--- calculator.py
+++ calculator.py
@@ -4,3 +4,10 @@ def add(a,b):
 def subtract(a,b):
     return a-b
 
+def multiply(a,b):
+    return a*b
+
+def division(a,b):
+    if b == 0:
+        return "Error! Can't divided by zero"
+    return a/b
